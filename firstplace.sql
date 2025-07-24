f282815153cf0d7c23052ea8e186e28bd68daeab
Ally Amaral <allygator242@gmail.com>
2025-07-24 19:17:07 -0400

    Add table exports and table schema scripts

---
+++ firstplace.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `firstplace` (
+  `id` int NOT NULL,
+  `horse_name` varchar(45) NOT NULL,
+  `year_raced` int NOT NULL,
+  `earnings` int NOT NULL,
+  `winning_length` double NOT NULL,
+  `winning_time` double NOT NULL,
+  PRIMARY KEY (`id`));
---
+++ horses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `horses` (
+  `id` int NOT NULL,
+  `horse_name` text NOT NULL,
+  `birthday` int NOT NULL,
+  `age` int NOT NULL,
+  `gender` text NOT NULL,
+  `color` text NOT NULL,
+  PRIMARY KEY (`id`)
+);
